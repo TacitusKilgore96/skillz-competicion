8b14a6789ec4180148e60f65df3db45f8d1371c6
Oliver Froberg <Oliver.h.froberg@gmail.com>
2026-08-17 09:17:27 +0200

    Fixed tracked files in git repo

--- .next/dev/server/chunks/ssr/_next-internal_server_app_teampage_page_actions_16d41st.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/teampage/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_teampage_page_actions_16d41st.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5DBXVVfPKaRZEjCFPTmRWyXclcFXtm7LRAJqhb7uGIo=\"\n}"
