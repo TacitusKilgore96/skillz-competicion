b57061d6e85b49b6d0ad045952c1d54add7a3cd2
Oliver Froberg <Oliver.h.froberg@gmail.com>
2026-08-26 18:37:52 +0200

    Refactor `Events` pages: extract event links into anchor elements, adjust layout structure, and split content into dynamic routes.

--- src/app/admin/events/[id]/page.tsx
+++ src/app/admin/events/[id]/page.tsx
@@ -15,23 +15,16 @@ export default function Page() {
 
 	return (
 		<AdminShell pageTitle={"Begivenheder"} currentPath={"/admin/events"}>
-			<div className={"p-8 h-full flex flex-col items-center gap-4 mx-auto max-w-300"}>
+			<div className={"p-4 h-full flex flex-col items-center gap-4 max-w-120 border-r border-gray-300"}>
 				<div className={"w-full flex justify-between"}>
 					<input className={textField()} placeholder={"Søg..."}/>
 
-					<button className={cn(button(), "uppercase font-semibold")}>+ Opret event</button>
+					<button className={cn(iconButton(), "uppercase font-semibold p-1 text-2xl")}>+</button>
 				</div>
 
-				<div className={cn(
-					card(),
-					"w-full h-full overflow-y-scroll flex flex-col p-2"
-				)}>
-					<div
-						className={"grid grid-cols-[1.25fr_1fr_1fr_.25fr] p-2 border-b border-gray-300 font-medium items-center"}>
+				<div className={"w-full h-full overflow-y-scroll flex flex-col p-2"}>
+					<div className={"flex flex-col p-2 border-b border-gray-300 font-medium"}>
 						<p>Navn</p>
-						<p>Dato</p>
-						<p>Status</p>
-						<p className={"text-right"}>Handlinger</p>
 					</div>
 
 					<AsyncDataRenderer
@@ -41,15 +34,8 @@ export default function Page() {
 
 						renderData={events => events!.map(event => (
 							<a key={event.id} href={`/admin/events/${event.id}`}
-							     className={"grid grid-cols-[1.25fr_1fr_1fr_.25fr] items-center p-2 border-b border-gray-300"}>
+							     className={"items-center px-2 py-4 border-b border-gray-300"}>
 								<p className={"font-medium"}>{event.name}</p>
-								<p className={"text-gray-600"}>{event.date}</p>
-								<p className={"text-gray-600"}>{event.status}</p>
-								<div className={"ml-auto"}>
-									<button className={iconButton()}>
-										<IconEdit/>
-									</button>
-								</div>
 							</a>
 						))}
 					/>
